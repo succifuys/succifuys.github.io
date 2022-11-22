8b658ff823bbdc42ad3b99e1c154a9bcf4daccec
succifuys <59948239+succifuys@users.noreply.github.com>
2022-11-22 17:55:49 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,9 @@
-/*
-	Escape Velocity by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
 (function($) {
 
 	var	$window = $(window),
 		$body = $('body');
 
-	// Breakpoints.
+
 		breakpoints({
 			xlarge:  [ '1281px',  '1680px' ],
 			large:   [ '981px',   '1280px' ],
@@ -17,14 +11,14 @@
 			small:   [ null,      '736px'  ]
 		});
 
-	// Play initial animations on page load.
+
 		$window.on('load', function() {
 			window.setTimeout(function() {
 				$body.removeClass('is-preload');
 			}, 100);
 		});
 
-	// Dropdowns.
+
 		$('#nav > ul').dropotron({
 			mode: 'fade',
 			noOpenerFade: true,
@@ -32,9 +26,7 @@
 			detach: false
 		});
 
-	// Nav.
 
-		// Title Bar.
 			$(
 				'<div id="titleBar">' +
 					'<a href="#navPanel" class="toggle"></a>' +
@@ -43,7 +35,7 @@
 			)
 				.appendTo($body);
 
-		// Panel.
+
 			$(
 				'<div id="navPanel">' +
 					'<nav>' +
